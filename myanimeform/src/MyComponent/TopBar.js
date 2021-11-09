67578c5c90513c3513fa52d819da8ff76749a5e6
jplox <rpawar@innominds.com>
2021-11-09 17:08:23 +0530

    commit on 'master'

--- myanimeform/src/MyComponent/TopBar.js
+++ myanimeform/src/MyComponent/TopBar.js
@@ -3,7 +3,7 @@ function TopBar(){
         <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-        <a className="navbar-brand" href="/">Metro Shoes</a>
+        <a className="navbar-brand" href="/">Metro Express Shoes</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
