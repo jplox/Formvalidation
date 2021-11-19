e88b9e820848082acfaa1c398c3eff50c67c3ad2
jplox <rpawar@innominds.com>
2021-11-19 15:36:57 +0530

    updated

--- myanimeform/src/MyComponent/LandingPage components/FeaturedProducts.js
+++ myanimeform/src/MyComponent/LandingPage components/FeaturedProducts.js
@@ -15,7 +15,7 @@ class FeaturedProducts extends React.Component {
                         <h2 class="section-title">Featured products</h2>
                         <div class="split">
                             {this.props.Featured.map((change) =>
-                                <a href="#" class="featured__item">
+                                <a href="/" class="featured__item">
                                     <img src={change.img} alt="" class="featured__img" />
                                     <p class="featured__details">
                                         <span class="price">{this.props.price}</span>{change.title}
